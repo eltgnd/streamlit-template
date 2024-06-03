7888d90bd504e95a2f1ea91a6b32755e870a0854
Val Eltagonde <63649722+eltgnd@users.noreply.github.com>
2024-06-03 09:12:09 +0800

    files

---
+++ pages/page.py
@@ -0,0 +1,15 @@
+# Packages
+import streamlit as st
+
+# Variables
+ss = st.session_state
+
+# Callable
+
+# Initialize
+page_title = ''
+page_icon = ':'
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+# Main
